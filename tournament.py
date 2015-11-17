2bc21172101d1e1f7e2124a1dd3ac852f48742d7
petrundiy2 <petrundiy1@gmail.com>
2015-11-17 11:54:54 +0300

    Fight! For survival! Death! On arrival!

--- tournament.py
+++ tournament.py
@@ -1,3 +1,4 @@
+__author__ = 'student'
 # coding: utf-8
 # license: GPLv3
 from enemies import *
@@ -14,39 +15,42 @@ def annoying_input_int(message =''):
 
 
 def game_tournament(hero, dragon_list):
+    n=0
     for dragon in dragon_list:
-        print('Вышел', dragon._color, 'дракон!')
-        while dragon.is_alive() and hero.is_alive():
+        print('Вышел', dragon._color, '!')
+        while dragon.is_alive()==True and hero.is_alive()==True:
             print('Вопрос:', dragon.question())
             answer = annoying_input_int('Ответ:')
 
             if dragon.check_answer(answer):
                 hero.attack(dragon)
-                print('Верно! \n** дракон кричит от боли **')
+                print('Верно! \n** враг кричит от боли **')
+                hero._experience+=10
             else:
                 dragon.attack(hero)
                 print('Ошибка! \n** вам нанесён удар... **')
-        if dragon.is_alive():
-            break
-        print('Дракон', dragon._color, 'повержен!\n')
+        if dragon.is_alive()==False:
+            n+=1
+            print('Враг', dragon._color, 'повержен!\n')
 
-    if hero.is_alive():
+    if hero.is_alive() and n==6:
         print('Поздравляем! Вы победили!')
         print('Ваш накопленный опыт:', hero._experience)
     else:
         print('К сожалению, Вы проиграли...')
+        print('Ваш накопленный опыт:', hero._experience)
 
 def start_game():
 
     try:
-        print('Добро пожаловать в арифметико-ролевую игру с драконами!')
+        print('Добро пожаловать в арифметико-ролевую игру с драконами и троллями!')
         print('Представьтесь, пожалуйста: ', end = '')
         hero = Hero(input())
 
-        dragon_number = 3
+        dragon_number = 6
         dragon_list = generate_dragon_list(dragon_number)
-        assert(len(dragon_list) == 3)
-        print('У Вас на пути', dragon_number, 'драконов!')
+        assert(len(dragon_list) == 6)
+        print('У Вас на пути', dragon_number, 'врагов!')
         game_tournament(hero, dragon_list)
 
     except EOFError:
