a67f9308976099fb1079b714d5511909c825af21
petrundiy2 <petrundiy1@gmail.com>
2015-11-17 11:50:01 +0300

    There is a hero to save my life...:P

--- hero.py
+++ hero.py
@@ -1,10 +1,20 @@
+__author__ = 'student'
 # coding: utf-8
 # license: GPLv3
 from gameunit import *
-
-#FIXME:
-"""В этом файле должен быть описан класс героя, унаследованный от Attacker
-Герой должен иметь 100 поинтов здоровья, атаку 50, опыт 0, имя, задаваемое в конструкторе
-Метод attack должен получать атрибут target и уменьшать его здоровье на величину атаки.
-
-"""
+class Hero (Attacker):
+    def __init__(self,name):
+        self._health = 100
+        self._attack = 50
+        self._name=name
+        self._experience=0
+    def attack(self,target):
+        target._health -= self._attack
+    def is_alive(self):
+        if self._health > 0:
+            return True
+        else:
+            return False
+    def gameOver(self):
+        if not Hero.is_alive(self):
+            pass
